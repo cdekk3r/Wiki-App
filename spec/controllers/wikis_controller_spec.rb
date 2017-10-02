2579c397c00d92913e82f15d8dfc9170dbd93e3f
Corey <cdekk3r@hotmail.com>
2017-10-02 00:18:08 +0000

    controller rspec error

--- spec/controllers/wikis_controller_spec.rb
+++ spec/controllers/wikis_controller_spec.rb
@@ -1,47 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe WikisController, type: :controller do
+  let(:my_wiki) { Wiki.create!(title: "New title", body: "New body", private: true) }
 
-  describe "GET #show" do
+  describe "GET #index" do
     it "returns http success" do
-      get :show
+      get :index
       expect(response).to have_http_status(:success)
+    end
+    
+    it "assigns Wiki.all to wiki" do
+      get :index
+      expect(assigns(:wikis)).to eq([my_wiki])
     end
   end
 
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET #show" do
+  #   it "returns http success" do
+  #     get :show
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET #new" do
+  #   it "returns http success" do
+  #     get :new
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
-  describe "GET #edit" do
-    it "returns http success" do
-      get :edit
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET #create" do
+  #   it "returns http success" do
+  #     get :create
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
-  describe "GET #update" do
-    it "returns http success" do
-      get :update
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET #edit" do
+  #   it "returns http success" do
+  #     get :edit
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
-  describe "GET #destroy" do
-    it "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET #update" do
+  #   it "returns http success" do
+  #     get :update
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+
+  # describe "GET #destroy" do
+  #   it "returns http success" do
+  #     get :destroy
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
 end
