48a64ee3ddf55f02abfd31cce848b9b43c451072
Corey <cdekk3r@hotmail.com>
2017-10-07 06:34:16 +0000

    role to user migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
-class User < ActiveRecord::Base
+class User < ActiveRecord::Base       
+  has_many :wikis, dependent: :destroy
+  
+  before_save {self.role ||= :standard }
+  
+  enum role: [:standard, :premium, :admin]
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
