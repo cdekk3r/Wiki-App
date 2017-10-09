f068124658482248071407e95742cd9cf7599617
Corey <cdekk3r@hotmail.com>
2017-10-09 16:17:47 +0000

    user spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ActiveRecord::Base       
   has_many :wikis, dependent: :destroy
   
-  before_save {self.role ||= :standard }
+  before_save { self.email = email.downcase if email.present? }
+  after_save {self.role ||= :standard }
   
   enum role: [:standard, :premium, :admin]
+  
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 8 }, presence: true
+  
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+    let(:user) { User.create!(email: "user@bloc.com", password: "password") }
+    
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_uniqueness_of(:email) }
+    it { is_expected.to allow_value("user@bloc.com").for(:email) }
+    
+    it { is_expected.to validate_presence_of(:password) }
+    it { is_expected.to validate_length_of(:password).is_at_least(8) }
+    
+    it { should have_many(:wikis) }
+    
+    describe "attributes" do
+        it "should have email attribute" do
+            expect(user).to have_attributes(email: "user@bloc.com")
+        end
+        
+        it "responds to role" do
+            expect(user).to respond_to(:role)
+        end
+        
+        # it "responds to admin?" do
+        #     expect(user).to respond_to(:admin?)
+        # end
+        
+        # it "responds to premium?" do
+        #     expect(user).to respond_to(:premium?)
+        # end
+        
+        # it "responds to standard?" do
+        #     expect(user).to respond_to(:standard?)
+        # end
+    end
 end
